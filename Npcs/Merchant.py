10386e7753a93056327b93d3a4798a2ee1b3b807
Your Name <you@example.com>
2023-05-09 08:49:08 +0200

    better numbers

--- Npcs/Merchant.py
+++ Npcs/Merchant.py
@@ -1,28 +1,30 @@
 from Player.Inventory import Inventory
 from Items.Weapon import Weapon
 from Items.Healpotion import HealthPotion
-
+import random
 
 class Merchant:
     def __init__(self, clazz: str):
         self.inventory = Inventory()
         self.shopWeapon = None
-        match clazz:
-            case "knight":
-                self.shopWeapon = Weapon("sword", 100)
-            case "mage":
-                self.shopWeapon = Weapon("staff", 100)
-            case "archer":
-                self.shopWeapon = Weapon("bow", 100)
-        if self.shopWeapon != None:
-            self.inventory.addItem("weapons", self.shopWeapon.showWeaponStats())
-            for i in range(10):
-                self.shopPotion = HealthPotion(2)
-                self.inventory.addItem("potions", self.shopPotion.showPotion())
+        for i in range(10):
+            match clazz:
+                case "knight":
+                    self.shopWeapon = Weapon("sword", self.numGenerator())
+                case "mage":
+                    self.shopWeapon = Weapon("staff", self.numGenerator())
+                case "archer":
+                    self.shopWeapon = Weapon("bow", self.numGenerator())
+            if self.shopWeapon != None:
+                self.inventory.addItem("weapons", self.shopWeapon.showWeaponStats())
+        for i in range(10):
+            self.shopPotion = HealthPotion(self.numGenerator(1,3))
+            self.inventory.addItem("potions", self.shopPotion.showPotion())
 
     def itemBought(self,key,id):
         self.boughtItem=self.inventory.showInventory(key,id)
         self.inventory.deleteItem(key,id)        
         return self.boughtItem
     
-
+    def numGenerator(self,start=30,end=200):
+        return random.randint(start,end)
